8ebeb046ed0f39bf74d0cc80d6d62db0f4df1668
alemughal <alimug38@gmail.com>
2021-05-27 20:24:22 +0500

    Creating Home Page and adding some css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React from "react";
+import HomePic from "../Images/homePic.jpg";
+import useWebAnimations, { lightSpeedInLeft } from "@wellyshen/use-web-animations";
 
-
-function Home () {
-    return (
-        <div>
-            <h1>Welcome to Home</h1>
-        </div>
-    )
+function Home() {
+  const { ref } = useWebAnimations({...lightSpeedInLeft});
+  return (
+    <div id="container">
+      <div id="content">
+        <h1 id="head1" ref={ref}>
+          Grow your bussiness with
+          <br />
+          <strong id="brandName">A.M Services</strong>
+        </h1>
+        <h4 id="head3">We are team of talented Developers</h4>
+        <p>
+          We are the team of talented developers Lorem ipsum odor amet,
+          consectetuer adipiscing elit. Penatibus auctor faucibus sollicitudin
+          laoreet hendrerit. Suspendisse dictumst finibus dolor maximus natoque
+          vulputate integer amet. Porttitor fusce nisi pharetra nam litora
+          interdum per! Conubia gravida cubilia dictumst justo ligula dignissim.
+          Nisl sollicitudin dis vivamus montes at cubilia volutpat cras
+          adipiscing.
+        </p>
+      </div>
+      <img src={HomePic} alt="" className="homePic" align="right" />
+    </div>
+  );
 }
 
-
 export default Home;
-
